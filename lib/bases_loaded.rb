3d2626604d614564d39cd6383ee884aed8a5efb8
Arian Ryan <arianryan@Arians-MacBook-Pro.local>
2017-09-22 18:01:49 -0400

    add the working code

--- lib/bases_loaded.rb
+++ lib/bases_loaded.rb
@@ -1,5 +1,52 @@
-require "bases_loaded/version"
+require 'bases_loaded/version'
+require 'bases_loaded/config'
 
 module BasesLoaded
-  # Your code goes here...
+
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    
+    def connection_pool
+      bases_loaded_connection_class.connection_pool
+    end
+
+    def retrieve_connection
+      bases_loaded_connection_class.retrieve_connection
+    end
+
+    def connected?
+      bases_loaded_connection_class.connected?
+    end
+
+    def remove_connection(klass = self)
+      bases_loaded_connection_class.remove_connection
+    end
+
+    def bases_loaded_config
+      @bases_loaded_config
+    end
+
+    def bases_loaded_base=(bases_loaded_base)
+      @bases_loaded_config = BasesLoaded::Config.new(bases_loaded_base)
+      self.bases_loaded_connection_class = @bases_loaded_config.connection_class
+      @bases_loaded_base = bases_loaded_base
+    end
+
+    def bases_loaded_base
+      @bases_loaded_base
+    end
+
+    def bases_loaded_connection_class=(klass)
+      @bases_loaded_connection_class = klass
+    end
+
+    def bases_loaded_connection_class
+      @bases_loaded_connection_class
+    end
+
+  end
+
 end
