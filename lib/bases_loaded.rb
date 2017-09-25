58cfd540f037b8049b202ec1ac1dcee540dbb2e7
Arian Ryan <arianryan@Arians-MacBook-Pro.local>
2017-09-25 12:41:42 -0400

    refactor in main module

--- lib/bases_loaded.rb
+++ lib/bases_loaded.rb
@@ -8,7 +8,7 @@ module BasesLoaded
   end
 
   module ClassMethods
-    
+
     def connection_pool
       bases_loaded_connection_class.connection_pool
     end
@@ -26,25 +26,20 @@ module BasesLoaded
     end
 
     def bases_loaded_config
-      @bases_loaded_config
+      @bases_loaded_config ||= BasesLoaded::Config.new(bases_loaded_base)
     end
 
     def bases_loaded_base=(bases_loaded_base)
-      @bases_loaded_config = BasesLoaded::Config.new(bases_loaded_base)
-      self.bases_loaded_connection_class = @bases_loaded_config.connection_class
       @bases_loaded_base = bases_loaded_base
     end
+    alias_method :bases_loaded_init, :bases_loaded_base=
 
     def bases_loaded_base
       @bases_loaded_base
     end
 
-    def bases_loaded_connection_class=(klass)
-      @bases_loaded_connection_class = klass
-    end
-
     def bases_loaded_connection_class
-      @bases_loaded_connection_class
+      @bases_loaded_connection_class ||= bases_loaded_config.connection_class
     end
 
   end
