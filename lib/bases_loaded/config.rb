9c15b5910923936008d7edebba00b04cc80cee54
Arian Ryan <arianryan@Arians-MacBook-Pro.local>
2017-09-25 12:41:13 -0400

    fix namespacing in config.rb

--- lib/bases_loaded/config.rb
+++ lib/bases_loaded/config.rb
@@ -1,54 +1,56 @@
-class Config
-  attr_reader :config_name
+module BasesLoaded
+  class Config
+    attr_reader :config_name
 
-  def self.connection_classes
-    @@connection_classes ||= {}
-  end
-
-  def self.register_connection_class(name, klass)
-    connection_classes[name] = klass
-  end
-
-  def initialize(config_name)
-    @config_name = config_name
-  end
-
-  def connection_class
-    @connection_class ||= begin
-      classes = self.class.connection_classes
-      classes[connection_class_name] || new_connection_class
+    def self.connection_classes
+      @@connection_classes ||= {}
     end
-  end
 
-  private
-
-  def params
-    environment_configs[env]
-  end
-
-  def connection_class_name
-    config_name.classify
-  end
-
-  def new_connection_class
-    Class.new(ActiveRecord::Base).tap do |klass|
-      BasesLoaded.const_set(connection_class_name, klass)
-      klass.abstract_class = true
-      klass.establish_connection(params)
-      self.class.register_connection_class(connection_class_name, klass)
+    def self.register_connection_class(name, klass)
+      connection_classes[name] = klass
     end
-  end
 
-  def all_configs
-    ActiveRecord::Base.configurations['bases_loaded']
-  end
+    def initialize(config_name)
+      @config_name = config_name
+    end
 
-  def environment_configs
-    all_configs[config_name]
-  end
+    def connection_class
+      @connection_class ||= begin
+        classes = self.class.connection_classes
+        classes[connection_class_name] || new_connection_class
+      end
+    end
 
-  def env
-    Rails.env
-  end
+    private
 
+    def params
+      environment_configs[env]
+    end
+
+    def connection_class_name
+      config_name.classify
+    end
+
+    def new_connection_class
+      Class.new(ActiveRecord::Base).tap do |klass|
+        BasesLoaded.const_set(connection_class_name, klass)
+        klass.abstract_class = true
+        klass.establish_connection(params)
+        self.class.register_connection_class(connection_class_name, klass)
+      end
+    end
+
+    def all_configs
+      ActiveRecord::Base.configurations['bases_loaded']
+    end
+
+    def environment_configs
+      all_configs[config_name]
+    end
+
+    def env
+      Rails.env
+    end
+
+  end
 end
