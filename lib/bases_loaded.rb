a079c3199ec8efe957648213d09131acddfe2cfe
Arian Ryan <arianryan@Arians-MacBook-Pro.local>
2017-09-28 17:21:25 -0400

    revert to instance vars; inherit connection w/hook

--- lib/bases_loaded.rb
+++ lib/bases_loaded.rb
@@ -5,6 +5,11 @@ module BasesLoaded
 
   def self.included(base)
     base.extend(ClassMethods)
+    # This ensures inheriting classes get the same connection pool.
+    base.define_singleton_method(:inherited) do |subclass|
+      super(subclass)
+      subclass.bases_loaded_init(base.bases_loaded_base)
+    end
   end
 
   module ClassMethods
@@ -26,24 +31,24 @@ module BasesLoaded
     end
 
     def bases_loaded_config
-      @@bases_loaded_config ||= BasesLoaded::Config.new(bases_loaded_base)
+      @bases_loaded_config ||= BasesLoaded::Config.new(bases_loaded_base)
     end
 
     def bases_loaded_base=(bases_loaded_base)
-      @@bases_loaded_base = bases_loaded_base
+      @bases_loaded_base = bases_loaded_base
     end
     alias_method :bases_loaded_init, :bases_loaded_base=
 
     def bases_loaded_base
-      @@bases_loaded_base
+      @bases_loaded_base
     end
 
     def bases_loaded_connection_class=(connection_class)
-      @@bases_loaded_connection_class = connection_class
+      @bases_loaded_connection_class = connection_class
     end
 
     def bases_loaded_connection_class
-      @@bases_loaded_connection_class ||= bases_loaded_config.connection_class
+      @bases_loaded_connection_class ||= bases_loaded_config.connection_class
     end
 
   end
